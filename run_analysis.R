54ef2e3d8e1822aec78a7a6e7293e3e57b01e43f
caowenjia <wc2477@columbia.edu>
2016-03-27 21:42:43 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,7 +17,7 @@ activityLabels[,2] <- as.character(activityLabels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
-# Extract only the data on mean and standard deviation
+# Extracts only the measurements on the mean and standard deviation for each measurement.
 featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
 featuresWanted.names <- features[featuresWanted,2]
 featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
@@ -36,7 +36,7 @@ testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testSubjects, testActivities, test)
 
-# merge datasets and add labels
+# merge train and testd, add labels
 allData <- rbind(train, test)
 colnames(allData) <- c("subject", "activity", featuresWanted.names)
 
